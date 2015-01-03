bbfe784947aff47f1aa00be41d4f858628cf8082
Ermanno Baschiera <ebaschiera@gmail.com>
2015-01-03 16:15:08 +0100

    added support to international stores added toast notification if url doesn't match an amazon link

--- app/src/main/java/com/ebaschiera/triplecamel/ShareActivity.java
+++ app/src/main/java/com/ebaschiera/triplecamel/ShareActivity.java
@@ -3,9 +3,11 @@ package com.ebaschiera.triplecamel;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
 import android.content.pm.*;
+import android.widget.Toast;
 import java.util.regex.*;
 import java.util.List;
 
@@ -26,9 +28,11 @@ public class ShareActivity extends Activity {
         Uri data = intent.getData();
 
         String amazon_share_text = intent.getStringExtra(Intent.EXTRA_TEXT);
-        if (amazon_share_text.matches(".*https://www.amazon.it/.*")) {
+        //if (amazon_share_text.matches(".*https://www\\.amazon\\.(?:[a-zA-Z]{2}|[a-zA-Z]{2}\\.[a-zA-Z]{2}|com)/.*")) {
+        if (amazon_share_text.matches(".*https://www\\.amazon\\.(?:ca|cn|de|es|fr|in|it|co\\.jp|co\\.uk|com)/.*")) {
             Log.d("triple", "It matches!");
-            Pattern p = Pattern.compile(".*(https://www.amazon.it/.*)");
+            Log.d("triple", amazon_share_text);
+            Pattern p = Pattern.compile(".*(https://www\\.amazon\\.(?:ca|cn|de|es|fr|in|it|co\\.jp|co\\.uk|com)/.*)");
             Matcher m = p.matcher(amazon_share_text);
             String amazon_share_url = "";
             while (m.find()) { // Find each match in turn; String can't do this.
@@ -36,6 +40,12 @@ public class ShareActivity extends Activity {
             }
             if (amazon_share_url == "") {
                 //return a warning and stop the intent
+                Context context = getApplicationContext();
+                CharSequence text = "Sorry, no Amazon link found. Triple Camel will work only sharing an Amazon URL.";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                finish();
             }
             amazon_share_url = Uri.encode(amazon_share_url);
             Uri camel_search_uri = Uri.parse("http://camelcamelcamel.com/search?sq=" + amazon_share_url);
@@ -52,9 +62,22 @@ public class ShareActivity extends Activity {
             if (isIntentSafe) {
                 startActivity(webIntent);
                 finish();
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Sorry, no suitable app found to open an URL.";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                finish();
             }
         } else {
             //return a warning and stop the intent
+            Context context = getApplicationContext();
+            CharSequence text = "Sorry, no Amazon link found. Triple Camel will work only sharing an Amazon URL.";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            finish();
         }
 
 
