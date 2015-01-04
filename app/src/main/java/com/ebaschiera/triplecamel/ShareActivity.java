5d1d95bba564d45c8fc730617a1bb1c5e24e5241
Ermanno Baschiera <ebaschiera@gmail.com>
2015-01-04 08:14:04 +0100

    fixed regex to match USA links

--- app/src/main/java/com/ebaschiera/triplecamel/ShareActivity.java
+++ app/src/main/java/com/ebaschiera/triplecamel/ShareActivity.java
@@ -28,11 +28,11 @@ public class ShareActivity extends Activity {
         Uri data = intent.getData();
 
         String amazon_share_text = intent.getStringExtra(Intent.EXTRA_TEXT);
+        Log.d("triple", amazon_share_text);
         //if (amazon_share_text.matches(".*https://www\\.amazon\\.(?:[a-zA-Z]{2}|[a-zA-Z]{2}\\.[a-zA-Z]{2}|com)/.*")) {
-        if (amazon_share_text.matches(".*https://www\\.amazon\\.(?:ca|cn|de|es|fr|in|it|co\\.jp|co\\.uk|com)/.*")) {
+        if (amazon_share_text.matches(".*http[s]?://www\\.amazon\\.(?:com|ca|cn|de|es|fr|in|it|co\\.jp|co\\.uk)/.*")) {
             Log.d("triple", "It matches!");
-            Log.d("triple", amazon_share_text);
-            Pattern p = Pattern.compile(".*(https://www\\.amazon\\.(?:ca|cn|de|es|fr|in|it|co\\.jp|co\\.uk|com)/.*)");
+            Pattern p = Pattern.compile(".*(http[s]?://www\\.amazon\\.(?:com|ca|cn|de|es|fr|in|it|co\\.jp|co\\.uk|com)/.*)");
             Matcher m = p.matcher(amazon_share_text);
             String amazon_share_url = "";
             while (m.find()) { // Find each match in turn; String can't do this.
