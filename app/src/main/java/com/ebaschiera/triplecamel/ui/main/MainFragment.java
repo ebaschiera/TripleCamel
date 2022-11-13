7af2a358c801e4ef50a6e2e690ade0f9105a2f95
Ermanno Baschiera <ermanno@filoblu.com>
2022-11-13 18:13:04 +0100

    optimizations suggested by android studio. hope it won't break anything

--- app/src/main/java/com/ebaschiera/triplecamel/ui/main/MainFragment.java
+++ app/src/main/java/com/ebaschiera/triplecamel/ui/main/MainFragment.java
@@ -1,13 +1,6 @@
 package com.ebaschiera.triplecamel.ui.main;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
@@ -16,12 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.ebaschiera.triplecamel.R;
 
 public class MainFragment extends Fragment {
-
-    private MainViewModel mViewModel;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -32,7 +26,7 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.main_fragment, container, false);
-        TextView about_text = (TextView) rootView.findViewById(R.id.innerTextViewAbout);
+        TextView about_text = rootView.findViewById(R.id.innerTextViewAbout);
         Spanned sp = Html.fromHtml(getString(R.string.about_this_app));
         about_text.setText(sp);
         about_text.setMovementMethod(LinkMovementMethod.getInstance());
@@ -42,8 +36,6 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 }
